6e26e55b758790869ca53a0768528d9fe5090982
Gustavo Henrique de Almeida Gonçalves <gustavo@faprender.org>
2018-05-27 19:05:39 -0300

    Update awesome-avatar.js
    Changing .load( to on('load', ...

--- awesome_avatar/static/awesome_avatar/awesome-avatar.js
+++ awesome_avatar/static/awesome_avatar/awesome-avatar.js
@@ -46,7 +46,7 @@ bind_preview = function(selector, config, e) {
     $(selector + '-select-area').empty();
     $(selector + '-select-area').append('<img />');
 //    $(selector + '-select-area img').show();
-    $(selector + '-select-area img').attr('src', image_data).load(function(){
+    $(selector + '-select-area img').attr('src', image_data).on('load', function(){
         $(this).unbind('load');
 
         var img_width = $(this).width();
