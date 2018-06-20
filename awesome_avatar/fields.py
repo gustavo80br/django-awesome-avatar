dc524a51988cbb7f1befc9c2943e06cd73831ae9
Gustavo Henrique de Almeida Gonçalves <gustavo@faprender.org>
2018-06-20 15:48:16 -0300

    Fix problem using the AvatarField in Admin

--- awesome_avatar/fields.py
+++ awesome_avatar/fields.py
@@ -40,18 +40,21 @@ class AvatarField(models.ImageField):
 
     def save_form_data(self, instance, data):
         # if data and self.width and self.height:
-        file_ = data['file']
-        if file_:
+        try:
+            file_ = data['file']
+            if file_:
+                image = Image.open(file_)
+                image = image.crop(data['box'])
+                image = image.resize((self.width, self.height), Image.ANTIALIAS)
 
-            image = Image.open(file_)
-            image = image.crop(data['box'])
-            image = image.resize((self.width, self.height), Image.ANTIALIAS)
+                content = BytesIO()
+                image.save(content, config.save_format, quality=config.save_quality)
 
-            content = BytesIO()
-            image.save(content, config.save_format, quality=config.save_quality)
+                file_name = u'{}.{}'.format(os.path.splitext(file_.name)[0], config.save_format)
 
-            file_name = u'{}.{}'.format(os.path.splitext(file_.name)[0], config.save_format)
-
-            # new_data = SimpleUploadedFile(file.name, content.getvalue(), content_type='image/' + config.save_format)
-            new_data = InMemoryUploadedFile(content, None, file_name, 'image/' + config.save_format, len(content.getvalue()), None)
-            super(AvatarField, self).save_form_data(instance, new_data)
+                # new_data = SimpleUploadedFile(file.name, content.getvalue(), content_type='image/' + config.save_format)
+                new_data = InMemoryUploadedFile(content, None, file_name, 'image/' + config.save_format,
+                                                len(content.getvalue()), None)
+                super(AvatarField, self).save_form_data(instance, new_data)
+        except:
+            super().save_form_data(instance, data)
